872dd4699dcfb094fc87a902960b39c987331938
luodejun <345288991@qq.com>
2020-08-07 09:48:12 +0800

    1. 修改配置文件

--- src/main/resources/db/新增任务调度表和新增任务调度明细表_2020-08-06_罗德俊.sql
+++ src/main/resources/db/新增任务调度表和新增任务调度明细表_2020-08-06_罗德俊.sql
@@ -1,24 +1,24 @@
 -- 任务调度表 TaskSchedulingManage
 CREATE TABLE TaskSchedulingManage(
-	id  INT(11) PRIMARY KEY AUTO_INCREMENT,
-	url VARCHAR(50) NOT NULL,
-	remark VARCHAR(100) NOT NULL,
-	temp1 VARCHAR(50),
-	temp2 VARCHAR(50),
-	temp3 VARCHAR(50)
+	id  INT(11) PRIMARY KEY AUTO_INCREMENT COMMENT '自增id',
+	url VARCHAR(50) NOT NULL COMMENT '地址路径',
+	remark VARCHAR(100) NOT NULL COMMENT '接口信息' ,
+	temp1 VARCHAR(50) COMMENT '备注字段1',
+	temp2 VARCHAR(50) COMMENT '备注字段2',
+	temp3 VARCHAR(50) COMMENT '备注字段3'
 );
 
 -- 任务调度明细表 TaskSchedulingDetailsInfo
 CREATE TABLE TaskSchedulingDetailsInfo(
-	id INT(11) PRIMARY KEY AUTO_INCREMENT,
-	url VARCHAR(100) NOT NULL,
-	start_time VARCHAR(30) NOT NULL,
-	end_time VARCHAR(30) NOT NULL,
-	flag VARCHAR(2) NOT NULL,
-	batch VARCHAR(20) NOT NULL,
-	temp1 VARCHAR(50),
-	temp2 VARCHAR(50),
-	temp3 VARCHAR(50)
+	id INT(11) PRIMARY KEY AUTO_INCREMENT  COMMENT '自增id',
+	url VARCHAR(100) NOT NULL  COMMENT '地址路径',
+	start_time VARCHAR(30) NOT NULL COMMENT '开始日期',
+	end_time VARCHAR(30) NOT NULL COMMENT '结束日期',
+	flag VARCHAR(2) NOT NULL COMMENT '0 fail 1 success',
+	batch VARCHAR(20) NOT NULL COMMENT '标识号',
+	temp1 VARCHAR(50) COMMENT '备注字段1',
+	temp2 VARCHAR(50) COMMENT '备注字段2',
+	temp3 VARCHAR(50) COMMENT '备注字段3'
 );
 -- 维护10个接口地址信息，及接口名称。
 INSERT INTO TaskSchedulingManage (url,remark) VALUES('/api/accounting/vehicle-stock-change','Vehicle_Stock');
