72a39c2237455299580c1db858b7cee21296ce2f
chengyabin <chengyabin@sinosoft.com.cn>
2020-09-18 13:00:53 +0800

    临沂 专项科目名称修改02

--- src/main/resources/modifyDb/0918_临沂专项科目名称修改.sql
+++ src/main/resources/modifyDb/0918_临沂专项科目名称修改.sql
@@ -1,5 +1,6 @@
 # 1151/10 修改科目名称
 UPDATE  subjectinfo SET  subject_name ='预付账款-集团外'  WHERE account = '100200001' AND all_subject LIKE '1151/';
+UPDATE  subjectinfo SET  subject_name ='预付账款-集团外-省内'  WHERE account = '100200001' AND all_subject LIKE '1151/10/';
 
 # 修改专项名称名称 鲁Q363CK押金qy冯
 UPDATE  specialinfo SET  special_name ='鲁Q363CK押金qy冯' ,special_namep ='鲁Q363CK押金qy冯'   WHERE account = '100200001' AND special_code LIKE 'WLDX0250889';
