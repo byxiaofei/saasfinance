8a29225344d8c3a4c45022943707a0c1cfe1e1ca
moran <15231500531@163.com>
2020-11-25 17:14:51 +0800

    本次更新点：1）解除了进项税额、金额、原值与增值税计算关系。优化之后，原值等于金额、增值税率和进项税额手工录入，净值依旧等于原值-减值准备-累计折旧2）更新了固定资产存放地点信息。

---
+++ src/main/resources/db/新增固定资产放置地点.sql
@@ -0,0 +1,30 @@
+INSERT INTO  codemanage (code_type,code_code,code_name,temp,order_by) VALUES
+('organization','4567','维修喷漆车间','存放地点',NULL)
+,('organization','4566','维修钣金车间','存放地点',NULL)
+,('organization','4565','维修机修车间','存放地点',NULL)
+,('organization','4564','二楼VIP休息室','存放地点',NULL)
+,('organization','4563','客服吧台','存放地点',NULL)
+,('organization','4562','二楼VIP休息室','存放地点',NULL)
+,('organization','4561','一楼客户休息室','存放地点',NULL)
+,('organization','4560','维修部办公室','存放地点',NULL)
+,('organization','4559','工具室','存放地点',NULL)
+,('organization','4558','配件部','存放地点',NULL)
+;
+INSERT INTO codemanage (code_type,code_code,code_name,temp,order_by) VALUES
+('organization','4557','服务站长办公室','存放地点',NULL)
+,('organization','4556','销售经理办公室','存放地点',NULL)
+,('organization','4555','销售内勤办公室','存放地点',NULL)
+,('organization','4554','销售顾问办公室','存放地点',NULL)
+,('organization','4553','网销办公室','存放地点',NULL)
+,('organization','4552','收款室','存放地点',NULL)
+,('organization','4551','客服部','存放地点',NULL)
+,('organization','4550','市场部','存放地点',NULL)
+,('organization','4549','财务部','存放地点',NULL)
+,('organization','4548','行政部','存放地点',NULL)
+;
+INSERT INTO codemanage (code_type,code_code,code_name,temp,order_by) VALUES
+('organization','4547','总经理办公室','存放地点',NULL)
+,('organization','4546','财务办公室','存放地点',NULL)
+,('organization','4545','综合办公室','存放地点',NULL)
+,('organization','4544','精算办公室','存放地点',NULL)
+;
