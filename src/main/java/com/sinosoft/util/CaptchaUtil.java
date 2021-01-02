1f7b14f0870001fdf35f4b06163896c023e0a094
Billionaire-LG <1515679827@qq.com>
2021-01-02 13:58:46 +0800

    解决年结问题

--- src/main/java/com/sinosoft/util/CaptchaUtil.java
+++ src/main/java/com/sinosoft/util/CaptchaUtil.java
@@ -7,6 +7,7 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.Random;
 
+import javax.imageio.ImageIO;
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
@@ -117,10 +118,10 @@ public final class CaptchaUtil
 
         // 转成JPEG格式
         ServletOutputStream out = response.getOutputStream();
-        JPEGImageEncoder encoder = JPEGCodec.createJPEGEncoder(out);
+        /*JPEGImageEncoder encoder = JPEGCodec.createJPEGEncoder(out);
         encoder.encode(bi);
-        out.flush();
-
+        out.flush();*/
+        ImageIO.write(bi,"jpg",out);
 
     }
 }
