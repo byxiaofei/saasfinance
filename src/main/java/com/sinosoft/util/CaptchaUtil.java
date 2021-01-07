f4476cdb2af3412244a4fa68dfe9f8897bb251c4
Billionaire-LG <1515679827@qq.com>
2021-01-07 14:36:31 +0800

    修改验证码版本

--- src/main/java/com/sinosoft/util/CaptchaUtil.java
+++ src/main/java/com/sinosoft/util/CaptchaUtil.java
@@ -14,8 +14,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.sun.image.codec.jpeg.JPEGCodec;
-import com.sun.image.codec.jpeg.JPEGImageEncoder;
 
 /**
 * @ClassName: CaptchaUtil 
